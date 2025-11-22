42b33732688fef2cf9c3d20f61cdfc9273a43709
Yassin-Kassem <yassinkassem29@gmail.com>
2025-11-22 19:45:12 +0200

    import fix

--- app.py
+++ app.py
@@ -10,6 +10,7 @@ import plotly.graph_objects as go
 import pandas as pd
 import numpy as np
 from datetime import datetime
+import os
 
 # =============================================================================
 # CONFIGURATION
